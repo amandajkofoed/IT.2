3fea6296c3fdf46d7c2655b3e5939fc7c759975c
Amanda Kofoed <amandakofoed@Amandas-MacBook-Pro-2.local>
2021-02-09 08:47:49 +0100

    Initial commit

---
+++ src/main/java/Amanda.java
@@ -0,0 +1,13 @@
+public class Amanda {
+
+    public void setHøjde(double højde) {
+        this.højde = højde;
+    }
+
+    public double getHøjde() {
+        return højde;
+    }
+
+    private double højde = 162;
+
+}
